17b9de7f667ebf7aa107c0cd8c73cb25371717a6
Kotaro Inoue <k.musaino@gmail.com>
2020-09-20 10:13:13 +0900

    Add rlibc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+extern crate rlibc;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
